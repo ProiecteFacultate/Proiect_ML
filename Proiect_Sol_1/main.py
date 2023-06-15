333dd2c72dea0aace724b092f384ba7b10a0c0f5
Mihai Razvan <MIHAIRAZVAN03@YAHOO.COM>
2023-06-16 00:10:00 +0300

    refactor + matricea de confuzie

--- Proiect_Sol_1/main.py
+++ Proiect_Sol_1/main.py
@@ -1,27 +1,46 @@
-import pandas as pd
 from pandas import read_csv
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
-import cv2
 import numpy as np
 from PIL import Image
+import pandas as pd
 
 def deschiderInitialaDeFisiere():
-    global directorCuDate, CSVdeAntrenare, CSVdeValidare, CSVdeTest, listaClaseCsvDeAntrenament, listaClaseCsvDeValidare
+    global directorCuDate, CSVdeAntrenare, CSVdeValidare, CSVdeTest, claseCsvDeAntrenament, claseCsvDeValidare, numeImaginiDeAntrenament, numeImaginiDeValidare, numeImaginiDeTest
     directorCuDate = "D:\Facultate\II\S_2\IA\Proiect_ML\Date_Proiect/"  # unde am salvate fisierele cu imagini si csvurile la mine in pc
 
     CSVdeAntrenare = read_csv(directorCuDate + "train.csv")
     CSVdeValidare = read_csv(directorCuDate + "val.csv")
     CSVdeTest = read_csv(directorCuDate + "test.csv")
 
-    listaClaseCsvDeAntrenament = CSVdeAntrenare["Class"]
-    listaClaseCsvDeValidare = CSVdeValidare["Class"]
+    claseCsvDeAntrenament = CSVdeAntrenare["Class"]
+    claseCsvDeValidare = CSVdeValidare["Class"]
+
+    numeImaginiDeAntrenament = CSVdeAntrenare["Image"]  # o lista cu numele imaginilor din csvul de antrenament
+    numeImaginiDeValidare = CSVdeValidare["Image"]  # o lista cu numele imaginilor din csvul de validare
+    numeImaginiDeTest = CSVdeTest["Image"]  # o lista cu numele imaginilor din csvul de test
 
 
 def transformaListaInValoriDiscrete(lista, nrIntervale):
     listaDupaTransformare = np.digitize(lista, nrIntervale)  #primeste o lista de valori si le discretizeaza (inlocuieste o valoare cu intervalul din care face parte)
     listaIndexataDeLaZero = listaDupaTransformare - 1   #digitise are indexarea de la 1 si facem -1 ca sa avem indexare de la 0
     return listaIndexataDeLaZero
+
+def construiesteMatriceaDeConfuzie():   #se face pentru datele de validare
+    matriceConfuzie = [[0 for j in range(96)] for i in range(96)]
+
+    for i in range(len(predictiiValidare)):
+        valoarePrezisa = int(predictiiValidare[i])
+        valoareReala = int(claseCsvDeValidare[i])
+        matriceConfuzie[valoareReala][valoarePrezisa] += 1
+
+    print("Matricea de confuzie:")
+    for i in range(len(matriceConfuzie)):
+        for j in range(len(matriceConfuzie[i])):
+            print(matriceConfuzie[i][j], end=" ")
+        print('\n')
+
+
 
 deschiderInitialaDeFisiere()
 valoareMaximaInterval = 256  #255 e val maxima pt ca pixelii au val maxima 255 si facem +1 pt ca e deschis la capete
@@ -30,46 +49,47 @@ capeteIntervale = np.linspace(0, valoareMaximaInterval, num=nrIntervale)
 
 #prelucram datele pt imaginile de antrenare
 trasaturiDateDeAntrenament = []
-listaImaginiCsvDeAntrenament = CSVdeAntrenare["Image"]  #o lista cu numele imaginilor din csvul de antrenament
-for i in range(len(listaImaginiCsvDeAntrenament)):
-    numeImagine = listaImaginiCsvDeAntrenament[i]
+for i in range(len(numeImaginiDeAntrenament)):
+    numeImagine = numeImaginiDeAntrenament[i]
     imagine = Image.open(directorCuDate + "train_images/" + numeImagine)
     imagineCaNpArray = np.array(imagine)         #vrem sa avem datele legate de pixeli sub forma unui array din numpy
     trasaturiDateDeAntrenament.append(imagineCaNpArray.flatten())     #folosim flateen ca sa facem arrayul sa fie 1D
 
-valoriPrelucrateAntrenament = transformaListaInValoriDiscrete(trasaturiDateDeAntrenament, capeteIntervale)
+trasaturiPrelucrateAntrenament = transformaListaInValoriDiscrete(trasaturiDateDeAntrenament, capeteIntervale)
 
 #prelucram datele pt imaginile de validare
-trasaturiDateDeValidare = []
-listaImaginiCsvDeValidare = CSVdeValidare["Image"]  #o lista cu numele imaginilor din csvul de validare
-for i in range(len(listaImaginiCsvDeValidare)):
-    numeImagine = listaImaginiCsvDeValidare[i]
+trasaturiImaginiDeValidare = []
+for i in range(len(numeImaginiDeValidare)):
+    numeImagine = numeImaginiDeValidare[i]
     imagine = Image.open(directorCuDate + "val_images/" + numeImagine)
     imagineCaNpArray = np.array(imagine)
-    trasaturiDateDeValidare.append(imagineCaNpArray.flatten())
+    trasaturiImaginiDeValidare.append(imagineCaNpArray.flatten())
 
-valoriPrelucrateValidare = transformaListaInValoriDiscrete(trasaturiDateDeValidare, capeteIntervale)
+trasaturiPrelucrateValidare = transformaListaInValoriDiscrete(trasaturiImaginiDeValidare, capeteIntervale)
 
 modelNaiveBayes = MultinomialNB()
-modelNaiveBayes.fit(valoriPrelucrateAntrenament, listaClaseCsvDeAntrenament)
+modelNaiveBayes.fit(trasaturiPrelucrateAntrenament, claseCsvDeAntrenament)
 
-predictiiValidare = modelNaiveBayes.predict(valoriPrelucrateValidare)
-acuratete = accuracy_score(listaClaseCsvDeValidare, predictiiValidare)
+predictiiValidare = modelNaiveBayes.predict(trasaturiPrelucrateValidare)
+acuratete = accuracy_score(claseCsvDeValidare, predictiiValidare)
+
 print("Acuratete de validare: " + str(acuratete))
+construiesteMatriceaDeConfuzie()
 
 #prelucram datele pt imaginile de test
-trasaturiDateDeTest = []
-listaImaginiCsvDeTest = CSVdeTest["Image"]  #o lista cu numele imaginilor din csvul de test
-for i in range(len(listaImaginiCsvDeTest)):
-    numeImagine = listaImaginiCsvDeTest[i]
+trasaturiImaginiDeTest = []
+for i in range(len(numeImaginiDeTest)):
+    numeImagine = numeImaginiDeTest[i]
     imagine = Image.open(directorCuDate + "test_images/" + numeImagine)
     imagineCaNpArray = np.array(imagine)
-    trasaturiDateDeTest.append(imagineCaNpArray.flatten())
+    trasaturiImaginiDeTest.append(imagineCaNpArray.flatten())
 
-valoriPrelucrateTest = transformaListaInValoriDiscrete(trasaturiDateDeTest, capeteIntervale)
+trasaturiPrelucrateTest = transformaListaInValoriDiscrete(trasaturiImaginiDeTest, capeteIntervale)
 
-predictiiTest = modelNaiveBayes.predict(valoriPrelucrateTest)
+predictiiTest = modelNaiveBayes.predict(trasaturiPrelucrateTest)
 
 fisierDePredictii = pd.DataFrame({"Image": CSVdeTest["Image"], "Class": predictiiTest})  #face fisierul unde pune clasa care a fost prezisa pt fiecare imagine
 fisierDePredictii.to_csv(directorCuDate + "submission.csv", index=False)
+
+
 
--- Proiect_Sol_2/main.py
+++ Proiect_Sol_2/main.py
@@ -1,91 +1,117 @@
 import pandas as pd
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
-import cv2
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
+from pandas import read_csv
+from PIL import Image
+
+def deschiderInitialaDeFisiere():
+    global directorCuDate, CSVdeAntrenare, CSVdeValidare, CSVdeTest, claseCsvDeAntrenament, claseCsvDeValidare, numeImaginiDeAntrenament, numeImaginiDeValidare, numeImaginiDeTest
+    directorCuDate = "D:\Facultate\II\S_2\IA\Proiect_ML\Date_Proiect/"  # unde am salvate fisierele cu imagini si csvurile la mine in pc
+
+    CSVdeAntrenare = read_csv(directorCuDate + "train.csv")
+    CSVdeValidare = read_csv(directorCuDate + "val.csv")
+    CSVdeTest = read_csv(directorCuDate + "test.csv")
+
+    claseCsvDeAntrenament = CSVdeAntrenare["Class"]
+    claseCsvDeValidare = CSVdeValidare["Class"]
+
+    numeImaginiDeAntrenament = CSVdeAntrenare["Image"]  # o lista cu numele imaginilor din csvul de antrenament
+    numeImaginiDeValidare = CSVdeValidare["Image"]  # o lista cu numele imaginilor din csvul de validare
+    numeImaginiDeTest = CSVdeTest["Image"]  # o lista cu numele imaginilor din csvul de test
 
 
+def classificaOSinguraImagine(tipDistanta, numarVecini, imagineCareTrabuieTestata):
+    if tipDistanta == 'Euclidian':
+        valoareDistanta = np.sqrt(np.sum(np.square(trasaturiDateDeAntrenament - imagineCareTrabuieTestata), axis=(1, 2, 3)))  # distanta intre 2 imagini; facem pe 3 axe pt ca imaginea e de forma nrImagini x nrPixeliPeRand x nrPixeliPeColoana x 3(RGB)
+    elif tipDistanta == 'Manhattan':
+        valoareDistanta = np.sum(np.abs(trasaturiDateDeAntrenament - imagineCareTrabuieTestata), axis=(1, 2, 3))
 
-class Knn_classifier:
-    def __init__(self, train_images, train_labels):
-        # Ex. 1
-        self.train_images = train_images
-        self.train_labels = train_labels
+    indiciSortati = np.argsort(valoareDistanta)  # indicii celor mai apropiati vecini sunt primii
+    aparitiiClase = [0 for x in range(96)]
 
-    def classify_image(self, test_image, num_neighbors, metric='l2'):
-        if metric == 'l2':    #euclidian
-            distances = np.sqrt(np.sum(np.square(self.train_images - test_image), axis=(1, 2, 3)))
-        elif metric == 'l1':  #manhaattan
-            distances = np.sum(np.abs(self.train_images - test_image), axis=(1, 2, 3))
-        else:
-            raise ValueError()
+    for i in range(numarVecini):
+        indice = indiciSortati[i]
+        clasa = claseCsvDeAntrenament[indice]
+        aparitiiClase[int(clasa)] += 1
 
-        best_indices = np.argsort(distances)
-        top_k_indices = best_indices[:num_neighbors].tolist()
-        tok_k_labels = np.array(self.train_labels)[top_k_indices].ravel()
-        counts = np.bincount(tok_k_labels)
-        pred_label = np.argmax(counts)
+    clasaCuNrMaxDeAparitii = -1
+    numarMaximAparitii = -1
+    for i in range(96):
+        if aparitiiClase[i] > numarMaximAparitii:
+            numarMaximAparitii = aparitiiClase[i]
+            clasaCuNrMaxDeAparitii = i
 
-        return pred_label
-
-    def classify_images(self, test_images, num_neighbors, metric='l2'):
-        num_test_images = test_images.shape[0]
-        predicted_labels = np.zeros((num_test_images), np.int8)
-
-        for i in range(num_test_images):
-            predicted_labels[i] = self.classify_image(test_images[i, :], num_neighbors=num_neighbors, metric=metric)
-
-        return predicted_labels
-
-    def accuracy_score(self, y_pred, y_true):
-        return np.sum(y_pred == y_true) / len(y_pred)
+    return clasaCuNrMaxDeAparitii
 
 
-def setup():
-    global data_directory, train_images, train_csv, val_csv, test_csv, train_labels, val_labels
-    data_directory = "D:\Facultate\II\S_2\IA\Proiect_ML\Date_Proiect/"  # unde am salvate fisierele cu imagini si csvurile
+def clasificaImagini(tipDistanta, numarVecini, trasaturiImagini):
+    numarImagini = len(trasaturiImagini)  # trasaturiImagini e de forma nrImagini x nrPixeliPeRand x nrPixeliPeColoana x 3(RGB)
+    clasePrezise = []
 
-    train_csv = pd.read_csv(data_directory + "train.csv")
-    val_csv = pd.read_csv(data_directory + "val.csv")
-    test_csv = pd.read_csv(data_directory + "test.csv")
+    for i in range(numarImagini):
+        clasaPrezisa = classificaOSinguraImagine(tipDistanta, numarVecini, trasaturiImagini[i])
+        clasePrezise.append(clasaPrezisa)
 
-    train_labels = train_csv["Class"]
-    val_labels = val_csv["Class"]
+    return clasePrezise
 
 
-setup()
-train_images = []
-train_labels = []
-scaler = MinMaxScaler()  # Create a MinMaxScaler instance for normalization
-for image_file, label in zip(train_csv["Image"], train_csv["Class"]):
-    image = cv2.imread(data_directory + "train_images/" + image_file)
-    image_as_array = np.array(image)
-    train_images.append(image_as_array)
-    train_labels.append(label)
+def calculeazaAcuratete(clasePrezise, claseReale):
+    numarPredictiiCorecte = 0
+    numarImagini = len(clasePrezise)  # clasePrezise e o lista cu clasele prezise pt fiecare imagine deci nrImagini = nrClasePrezise = nrClaseReale
 
-knn_classifier = Knn_classifier(train_images=train_images, train_labels=train_labels)
+    for i in range(numarImagini):
+        if clasePrezise[i] == claseReale[i]:
+            numarPredictiiCorecte += 1
 
-val_images = []
-val_labels = []
-for image_file, label in zip(val_csv["Image"], val_csv["Class"]):
-    image = cv2.imread(data_directory + "val_images/" + image_file)
-    image_as_array = np.array(image)
-    val_images.append(image_as_array)
-    val_labels.append(label)
+    acuratete = numarPredictiiCorecte / numarImagini
+    return acuratete
 
-test_images = []
-for image_file in test_csv["Image"]:
-    image = cv2.imread(data_directory + "test_images/" + image_file)
-    image_as_array = np.array(image)
-    test_images.append(image_as_array)
 
-val_images = np.array(val_images)    #altfel e list in loc de numpyarray si da eroarea ca nu are attribute shape
-val_predictions = knn_classifier.classify_images(val_images, num_neighbors=498, metric='l2')
-acc_score = knn_classifier.accuracy_score(val_predictions, val_labels)
-print("Accuracy score: " + str(acc_score))
+def construiesteMatriceaDeConfuzie():   #se face pentru datele de validare
+    matriceConfuzie = [[0 for j in range(96)] for i in range(96)]
 
-test_images = np.array(test_images)
-test_predictions = knn_classifier.classify_images(test_images, num_neighbors=81, metric='l2')
-submission_df = pd.DataFrame({"Image": test_csv["Image"], "Class": test_predictions})
-submission_df.to_csv(data_directory + "submission.csv", index=False)
+    for i in range(len(predictiiValidare)):
+        valoarePrezisa = int(predictiiValidare[i])
+        valoareReala = int(claseCsvDeValidare[i])
+        matriceConfuzie[valoareReala][valoarePrezisa] += 1
+
+    print("Matricea de confuzie:")
+    for i in range(len(matriceConfuzie)):
+        for j in range(len(matriceConfuzie[i])):
+            print(matriceConfuzie[i][j], end=" ")
+        print('\n')
+
+
+##################### MAAAIIIIIINNNNN #############
+
+deschiderInitialaDeFisiere()
+
+trasaturiDateDeAntrenament = []
+for i in range(len(numeImaginiDeAntrenament)):
+    numeImagine = numeImaginiDeAntrenament[i]
+    imagine = Image.open(directorCuDate + "train_images/" + numeImagine)
+    imagineCaNpArray = np.array(imagine)  # vrem sa avem datele legate de pixeli sub forma unui array din numpy
+    trasaturiDateDeAntrenament.append(imagineCaNpArray)
+
+trasaturiDateDeValidare = []
+for i in range(len(numeImaginiDeValidare)):
+    numeImagine = numeImaginiDeValidare[i]
+    imagine = Image.open(directorCuDate + "val_images/" + numeImagine)
+    imagineCaNpArray = np.array(imagine)  # vrem sa avem datele legate de pixeli sub forma unui array din numpy
+    trasaturiDateDeValidare.append(imagineCaNpArray)
+
+trasaturiDateDeTest = []
+for i in range(len(numeImaginiDeTest)):
+    numeImagine = numeImaginiDeTest[i]
+    imagine = Image.open(directorCuDate + "test_images/" + numeImagine)
+    imagineCaNpArray = np.array(imagine)  # vrem sa avem datele legate de pixeli sub forma unui array din numpy
+    trasaturiDateDeTest.append(imagineCaNpArray)
+
+predictiiValidare = clasificaImagini('Euclidian', 498, trasaturiDateDeValidare)
+acuratete = calculeazaAcuratete(predictiiValidare, claseCsvDeValidare)
+
+print("Acuratete de validare: " + str(acuratete))
+construiesteMatriceaDeConfuzie()
+
+predictiiTest = clasificaImagini('Euclidian', 81, trasaturiDateDeTest)
+fisierDePredictii = pd.DataFrame({"Image": CSVdeTest["Image"], "Class": predictiiTest})  #face fisierul unde pune clasa care a fost prezisa pt fiecare imagine
+fisierDePredictii.to_csv(directorCuDate + "submission.csv", index=False)
